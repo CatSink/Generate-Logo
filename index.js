2d345c345177524b2a8993c6bb3c8fcf7659d2dc
CatSink <raindropwebweaver@live.com>
2023-06-29 17:23:05 -0400

    updated version of inquirer still having issues with undefined

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const {Circle, Rectangle, Triangle} = require('./shapes')
-const Logo = require('./logo');
-const { generateKey } = require('crypto');
-const questions = [
-    {
-        type: 'input',
-        message: 'Please enter less than 3 characters for your logo',
-        name:'text',
-
-    },
-    {
-        type: 'list',
-        message: 'Select a shape for your Logo',
-        options:[{Circle,Triangle,Rectangle}],
-        name:'shape',
-    },
-    {
-        type: 'list',
-        message: 'Please select a background color for your Logo',
-        name:'color',
-        options:[red,blue,green,black,white,orange,yellow,purple,gray]
-        
-    },
-    {
-        type: 'input',
-        message: 'Please enter less than 3 characters for your logo',
-        name:'text',
-    },
-    {
-        type: 'input',
-        message: 'Please enter less than 3 characters for your logo',
-        name:'text',
-    }
-]
+const logo = require('./lib/logo');
+const selector = require('./lib/shapes');
+const CLI = require('./lib/cli');
+const cli= new CLI();
+cli.run();
